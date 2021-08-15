1a99171de7888d3c58120e178e3d90d48d01d2a2
Weera Wu <ing@oozou.com>
2021-08-15 12:14:32 +0700

    Improve drawing performance

--- public/main.js
+++ public/main.js
@@ -41,19 +41,6 @@ class Turtle {
     this.state = new State(x, y, a);
   }
 
-  drawSelf() {
-    this.ctx.save();
-    this.ctx.beginPath();
-    this.ctx.translate(this.state.x, this.state.y);
-    this.ctx.rotate((Math.PI/180) * this.state.a);
-    this.ctx.moveTo(0, 0);
-    this.ctx.lineTo(-20, 10);
-    this.ctx.lineTo(-20, -10);
-    this.ctx.closePath();
-    this.ctx.fill();
-    this.ctx.restore();
-  }
-
   move(d, a) {
     const newState = new State(
       this.state.x + (d * Math.cos((Math.PI/180) * this.state.a)),
@@ -63,7 +50,6 @@ class Turtle {
 
     if (!State.samePosition(this.state, newState)) {
       this.ctx.lineTo(newState.x, newState.y);
-      this.ctx.stroke();
     }
 
     this.state = newState;
@@ -79,6 +65,29 @@ class Turtle {
 
   turnRight(a) {
     this.move(0, a);
+  }
+
+  draw() {
+    this.drawPath();
+    this.drawSelf();
+  }
+
+  drawPath() {
+    this.ctx.stroke();
+    this.ctx.closePath();
+  }
+
+  drawSelf() {
+    this.ctx.save();
+    this.ctx.beginPath();
+    this.ctx.translate(this.state.x, this.state.y);
+    this.ctx.rotate((Math.PI/180) * this.state.a);
+    this.ctx.moveTo(0, 0);
+    this.ctx.lineTo(-20, 8);
+    this.ctx.lineTo(-20, -8);
+    this.ctx.closePath();
+    this.ctx.fill();
+    this.ctx.restore();
   }
 }
 
@@ -100,7 +109,7 @@ function main() {
   // turtle.forward(100);
   // turtle.turnRight(90);
   // turtle.forward(200);
-  // turtle.drawSelf();
+  // turtle.draw();
 }
 
 if (document.readyState === 'loading') {
